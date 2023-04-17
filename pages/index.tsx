c575790ccca098f72f2faa66642d5a9df7c45499
androidblog <androidblog@yandex.ru>
2023-04-17 18:33:07 +0400

    create color variables

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Rubik } from "next/font/google";
+import styles from "@/styles/Home.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const rubil = Rubik({ subsets: ["latin"], weight: ["300", "400", "500"] });
 
 export default function Home() {
   return (
@@ -14,9 +14,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main >
+      <main>
         
       </main>
     </>
-  )
+  );
 }
