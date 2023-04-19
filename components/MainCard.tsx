ae47e4fb0d9463b5b42c432db2b7870916ecdf3b
androidblog <androidblog@yandex.ru>
2023-04-19 16:48:53 +0400

    add periods to main card

--- components/MainCard.tsx
+++ components/MainCard.tsx
@@ -1,16 +1,38 @@
 import React from "react";
 import cardStyle from "@/styles/Card.module.css";
+import pageStyles from "@/styles/Home.module.css";
 import Image from "next/image";
+import Link from "next/link";
 
-export default function MainCard({ userName, cardName, avatarSrc }: { userName: string, cardName: string, avatarSrc: string }) {
+const periods = ["daily", "weekly", "monthly"];
+
+export default function MainCard({
+  userName,
+  cardName,
+  avatarSrc,
+  color,
+}: {
+  userName: string;
+  cardName: string;
+  avatarSrc: string;
+  color: string;
+}) {
   return (
-    <div className={`rounded ${cardStyle[cardName]} ${cardStyle['main-card-container']}`}>
-      <div className={cardStyle['main-card-header']}>
+    <div
+      className={`${cardStyle["main-card-container"]} rounded ${pageStyles[cardName]} ${cardStyle[color]} `}
+    >
+      <div className={cardStyle["main-card-header"]}>
         <Image src={avatarSrc} alt="avatar" width={78} height={78} />
         <p>Report for</p>
         <h1>{userName}</h1>
       </div>
-      .
+      <div className={cardStyle["main-card-body"]}>
+        {periods.map((period) => (
+          <Link key={period} href="#">
+            {period}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
