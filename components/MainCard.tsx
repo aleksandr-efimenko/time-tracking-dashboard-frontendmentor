985c82107fd99a965b1f806b0f6eb065db6c208a
androidblog <androidblog@yandex.ru>
2023-04-21 13:23:45 +0400

    add skeleton loading for main card

--- components/MainCard.tsx
+++ components/MainCard.tsx
@@ -1,49 +1,113 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import pageStyles from "@/styles/Home.module.css";
 import Image from "next/image";
 import Link from "next/link";
 import mainCardStyles from "@/styles/MainCard.module.css";
+import { Profile } from "@/pages/api/profile";
 
 const periods = ["daily", "weekly", "monthly"];
 
 export default function MainCard({
-  userName,
-  cardName,
-  avatarSrc,
-  color,
   selectedPeriod,
-  setSelectedPeriod
+  setSelectedPeriod,
 }: {
-  userName: string;
-  cardName: string;
-  avatarSrc: string;
-  color: string;
   selectedPeriod: string;
   setSelectedPeriod: Function;
 }) {
+  const [profileData, setProfileData] = useState<Profile>({} as Profile);
+  const [isLoadingProfile, setIsLoadingProfile] = useState<boolean>(false);
+  useEffect(() => {
+    setIsLoadingProfile(true);
+    setTimeout (() => {
+      
+    fetch("/api/profile")
+      .then((res) => res.json())
+      .then((data) => {
+        setProfileData(data);
+        setIsLoadingProfile(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsLoadingProfile(false);
+      });
+
+    }, 1000);
+  }, []);
+
+  if (isLoadingProfile) {
+    return <SkeletonMainCard />;
+  }
+
   return (
     <div
-      className={`${mainCardStyles["main-card-container"]} ${pageStyles[cardName]}`}
+      className={`${mainCardStyles["main-card-container"]} ${pageStyles["main-card"]}`}
     >
-      <div className={`${mainCardStyles["main-card-header"]} ${mainCardStyles[color]}`}>
-        <div className={mainCardStyles['user-info-container']}>
-          <div className={mainCardStyles['avatar-container']}>
-            <Image  src={avatarSrc} alt="avatar" fill />
+      <div
+        className={`${mainCardStyles["main-card-header"]} ${
+          mainCardStyles[profileData.color]
+        }`}
+      >
+        <div className={mainCardStyles["user-info-container"]}>
+          <div className={`${mainCardStyles["avatar-container"]} `}>
+            <Image
+              src={profileData.avatarSrc}
+              alt="avatar"
+              fill
+              placeholder="empty"
+              blurDataURL=""
+            />
           </div>
-          <div className={mainCardStyles['user-info-text']}>
+          <div className={mainCardStyles["user-info-text"]}>
             <p>Report for</p>
-            <h1 className={mainCardStyles["main-card-container-header"]}>{userName}</h1>
+            <h1 className={`${mainCardStyles["main-card-container-header"]} `}>
+              {profileData.userName}
+            </h1>
           </div>
         </div>
       </div>
       <div className={mainCardStyles["main-card-body"]}>
         {periods.map((period) => (
-          <Link 
-          onClick={() => setSelectedPeriod(period)}
-          className={period === selectedPeriod ? mainCardStyles['active-period'] : ''} 
-          key={period} href="#">
+          <Link
+            onClick={() => setSelectedPeriod(period)}
+            className={
+              period === selectedPeriod ? mainCardStyles["active-period"] : ""
+            }
+            key={period}
+            href="#"
+          >
             {period}
           </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export function SkeletonMainCard() {
+  return (
+    <div
+      className={`${mainCardStyles["main-card-container"]} ${pageStyles["main-card"]}`}
+    >
+      <div
+        className={`${mainCardStyles["main-card-header"]} ${mainCardStyles["blue"]}`}
+      >
+        <div className={mainCardStyles["user-info-container"]}>
+          <div className={`${mainCardStyles["avatar-container"]} `}>
+            <div className={`${mainCardStyles["avatar-skeleton"]} ${mainCardStyles["skeleton"]}`} /> 
+          </div>
+          <div className={mainCardStyles["user-info-text"]}>
+            <div className={`${mainCardStyles["user-info-text-skeleton-paragraph"]}  ${mainCardStyles["skeleton"]}`}/>
+            <div className={`${mainCardStyles["user-info-text-skeleton-heading"]}  ${mainCardStyles["skeleton"]}`} />
+            <div className={`${mainCardStyles["user-info-text-skeleton-heading"]}  ${mainCardStyles["skeleton"]}`} />
+          </div>
+        </div>
+      </div>
+      <div className={mainCardStyles["main-card-body"]}>
+        {periods.map((period) => (
+          <div
+            key={period}
+            className={`${mainCardStyles["user-info-text-skeleton-paragraph"]}  ${mainCardStyles["skeleton"]} ${mainCardStyles["active-period"]}`}
+          />
         ))}
       </div>
     </div>
